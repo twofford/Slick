0be51a0547d55d575dad9f1e95e9a5e17d09bb34
Taylor Wofford <taylor.wofford@gmail.com>
2020-04-06 19:01:40 -0400

    added entry file...again...again...again

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
-const Root = ({ store }) => (
+const Root = () => (
     <Provider store={store}>
         <HashRouter>
-            <div></div>
+            <div>Slick</div>
         </HashRouter>
     </Provider>
 );
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,1 +1,9 @@
-<div></div>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from "./store/store";
+import Root from "./components/root";
+
+document.addEventListener('DOMContentLoaded', () => {
+    root = document.getElementById('root')
+    ReactDOM.render(<Root/>, root)
+});
