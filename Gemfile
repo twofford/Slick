d7a58536886170e4c1cb408577213b0ed1e79191
Taylor Wofford <taylor.wofford@gmail.com>
2020-04-06 14:09:15 -0400

    finished backend user auth

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'pry'
   gem 'pry-rails'
+  gem 'annotate'
 end
 
 group :test do
---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+
+    attr_reader :password
+
+    validates :email, :username, :password_digest, :session_token, null: false
+
+    validates :password, length: { minimum: 8, allow_nil: true }
+    
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email)
+        @user = User.find_by(email: email)
+        @user && @user.is_password?(password) ? user : nil
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.pasword_digest).is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+end
