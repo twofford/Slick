37136bac01bd5234bf7f81e90da19d48f2087504
Taylor Wofford <taylor.wofford@gmail.com>
2020-07-25 23:45:48 -0400

    add online_status to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_154604) do
+ActiveRecord::Schema.define(version: 2020_07_26_025216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_154604) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "online_status"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
